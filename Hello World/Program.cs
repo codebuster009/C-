ea8d4d76706b6812b7924f31cbd367b5d01b7a38
kartavyaKloud <kartavya@kloudone.com>
2023-12-10 21:04:40 +0530

    First Program

---
+++ Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWorld
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            Console.WriteLine(number);
+        }
+    }
+    }
